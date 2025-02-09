cb93cf548d91a734d5f412026b2ff2afd6686386
Robert Whisenhunt <dahweirdacus@gmail.com>
2025-02-08 22:34:44 -0500

    Did some more things that were missing, still a work in progress.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set("layout", "./layouts/layout"); // not at views root
  *************************/
 app.use(static);
 // Index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute);
 // Intentional Error route for Task 3
--- utilities/index.js
+++ utilities/index.js
@@ -94,4 +94,10 @@ Util.formatVehicleDetails = function(vehicle) {
   return vehicleHtml;
 };
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 module.exports = Util;
