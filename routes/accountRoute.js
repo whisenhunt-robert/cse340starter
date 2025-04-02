5d255382007eb91150b6451f654b379ac830041b
Robert Whisenhunt <dahweirdacus@gmail.com>
2025-04-02 17:05:37 -0400

    More work in progress for Week 7. Hope to be onto Week 8 very soon.

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,21 +3,38 @@ const express = require("express")
 const router = express.Router()
 const utilities = require('../utilities')
 const accountController = require('../controllers/accountController')
+const regValidate = require('../utilities/account-validation')
 
 // Deliver Login View
-router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get("/login",
+  utilities.handleErrors(accountController.buildLogin))
 
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-// Route to register account
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the POST registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
 
 // Error handling middleware
 router.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send('Something went wrong!');
 });
+
+// Process the login attempt
+router.post(
+  "/login",
+  regValidate.loginRules(),
+  regValidate.checkLoginData,
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
 
 // Export the router
 module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const utilities = require('./utilities');
 const session = require("express-session")
 const pool = require('./database/')
 const accountRoute = require('./routes/accountRoute');
+const bodyParser = require("body-parser")
 
 /* ***********************
  * Middleware
@@ -37,6 +38,9 @@ app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 /* ***********************
  * View Engine and Templates
